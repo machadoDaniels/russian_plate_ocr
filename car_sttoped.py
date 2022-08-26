7f69c224703bd2038e5575448f56e898beac19f6
danielmpedrozo <108821459+danielmpedrozo@users.noreply.github.com>
2022-08-26 18:43:46 -0300

    2

--- car_sttoped.py
+++ car_sttoped.py
@@ -5,19 +5,23 @@ import imutils
 from matplotlib import pyplot as plt
 import easyocr
 import numpy as np
+from PIL import Image
+import pytesseract
+
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
 
 # Create our body classifier
 plate_classifier = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
 
 # Initiate video capture for video file
-frame = cv2.imread('images/Cars1.png')
+frame = cv2.imread('images/Cars9.png')
 
 
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
 edged = cv2.Canny(bfilter, 30, 200)
 
-plate = plate_classifier.detectMultiScale(gray, 1.1, 3)
+plate = plate_classifier.detectMultiScale(gray, 1.1, 2)
 
 keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(keypoints)
@@ -33,22 +37,31 @@ contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
 # Extract bounding boxes for any bodies identified
 
+try:
+    cropped_image = frame[plate[0][1]:plate[0][1]+plate[0][3], plate[0][0]: plate[0][0] + plate[0][2]]
+
+except:
+    for (x, y, w, h) in plate:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+else:
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(cropped_image)
+    text = result[0][-2]
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for (x, y, w, h) in plate:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, text=text, org=(x, y), fontFace=font, fontScale=1, color=(0, 255, 0), thickness=2,
+                    lineType=cv2.LINE_AA)
+
+# cropped_image = np.array(cropped_image)
+# text = pytesseract.image_to_string(cropped_image)
+# print(text)
 
 
-cropped_image = frame[plate[0][1]:plate[0][1]+plate[0][3], plate[0][0]: plate[0][0] + plate[0][2]]
-reader = easyocr.Reader(['en'])
-result = reader.readtext(cropped_image)
-print(result)
 
-text = result[0][-2]
-font = cv2.FONT_HERSHEY_SIMPLEX
 
-for (x, y, w, h) in plate:
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.putText(frame, text=text, org=(x, y), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)
 
 
 plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 plt.show()
-
-
--- teste_carros3.py
+++ teste_carros3.py
@@ -17,7 +17,7 @@ def rescale_frame(frame_input, percent=75):
 plate_classifier = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
 
 # Initiate video capture for video file
-cap = cv2.VideoCapture('License plate video.mp4')
+cap = cv2.VideoCapture('Video Analytics based License Plate Recognition - Entry_ Exit.mp4')
 
 
 # Loop once video is successfully loaded
@@ -26,33 +26,35 @@ while cap.isOpened():
     sleep(.05)
     # Read first frame
     ret, frame = cap.read()
-    frame = rescale_frame(frame, 50)
+    #frame = rescale_frame(frame, 50)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
-    edged = cv2.Canny(bfilter, 30, 200)
 
     # Pass frame to our car classifier
     plate = plate_classifier.detectMultiScale(gray, 1.1, 3)
-    if len(plate) != 0:
+    if len(plate) == 1:
+        if len(plate[0]) == 4:
+            bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+            edged = cv2.Canny(bfilter, 30, 200)
 
-        # Extract bounding boxes for any bodies identified
+            # Extract bounding boxes for any bodies identified
 
-        keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        contours = imutils.grab_contours(keypoints)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+            keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours = imutils.grab_contours(keypoints)
+            contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
 
 
-        cropped_image = frame[plate[0][1]:plate[0][1] + plate[0][3], plate[0][0]: plate[0][0] + plate[0][2]]
-        reader = easyocr.Reader(['en'])
-        result = reader.readtext(cropped_image)
+            cropped_image = frame[plate[0][1]:plate[0][1] + plate[0][3], plate[0][0]: plate[0][0] + plate[0][2]]
+            reader = easyocr.Reader(['en'])
+            result = reader.readtext(cropped_image)
 
-        text = result[0][-2]
-        font = cv2.FONT_HERSHEY_SIMPLEX
+            text = result[0][-2]
+            font = cv2.FONT_HERSHEY_SIMPLEX
 
-        for (x, y, w, h) in plate:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            cv2.putText(frame, text=text, org=(x, y), fontFace=font, fontScale=1, color=(0, 255, 0), thickness=2,
-                        lineType=cv2.LINE_AA)
+            for (x, y, w, h) in plate:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                cv2.putText(frame, text=text, org=(x, y), fontFace=font, fontScale=1, color=(0, 255, 0), thickness=2,
+                            lineType=cv2.LINE_AA)
+
 
     cv2.imshow('Cars', frame)
 
